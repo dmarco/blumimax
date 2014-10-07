4a3dfa3df0833a9262e15b7bbc35fb6f0826d1c8
Damián Marcote <dmarcote2004@yahoo.com.ar>
2014-10-07 01:33:19 -0300

    Modify - Metodo Show

--- app/controllers/CategoriasController.php
+++ app/controllers/CategoriasController.php
@@ -50,82 +50,28 @@ class CategoriasController extends BaseController {
 	public function show()
 	{
 
-		/*$root = Categoria::all()->toHierarchy();
-		return View::make('admin.categorias.index')
-			->with('categorias', $root);*/
-
-		// echo '<pre>';
-		// $json = json_decode($root);
-		// echo json_encode($json, JSON_PRETTY_PRINT);
-		// echo '</pre>';
-		
-		/*
-		$tree = array();
-		$root = DB::table('categorias')->where('parent_id', '=', NULL)->get();
-
-    foreach($root as $cat){
-    	$categoria = array();
-    	$categoria['id'] = $cat->id;
-	    $categoria['name'] = $cat->name;
-	    $children = DB::table('categorias')->where('parent_id', '=', $cat->id)->get();
-	    
-	    if(count($children > 0)) {
-        $categoria['children'] = $this->getChildren($children);
-	    }else{
-        $categoria['children'] = array();
-	    }
-
-	    $tree[] = $categoria;
-    }
-
-    $node = json_encode($tree);
-
-    return View::make('admin.categorias.index')
-			->with('categorias', $node);
-		*/
-
 		$root = DB::table('categorias')->where('parent_id', '=', NULL)->get();
 		$html = '<ul class="list-group">';
     foreach($root as $cat){
     	
+			$children = DB::table('categorias')->where('parent_id', '=', $cat->id)->get();
     	$html .= '<li class="list-group-item">';
+    	$html .= '<span class="badge">'.count($children).'</span>';
 			$html .= '<a href="/delete/'.$cat->id.'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> </a>';
 			$html .= $cat->name;
 
-			$children = DB::table('categorias')->where('parent_id', '=', $cat->id)->get();
-
-			if(count($children > 0)) {
-        $html .= $this->getChildrenHtml($children);
-	    }else{
-        $html .= 'asdasdasdasdasdas';
+			if(count($children) > 0) {
+        $html .= $this->getChildren($children);
 	    }
 
     }
     $html .= '</ul>';
+    
     return View::make('admin.categorias.index')
 			->with('html', $html);
 
-
 	}
   
-	public function getChildrenHtml($children) 
-  {
-  	$subhtml = '<ul class="list-group">';
-    foreach($children as $child){
-  		$subhtml .= '<li class="list-group-item">';
-  		$subhtml .= '<a href="/delete/'.$child->id.'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> </a>';
-			$subhtml .= $child->name;
-			$children = DB::table('categorias')->where('parent_id', '=', $child->id)->get();
-
-      if(count($children > 0)) {
-        $subhtml .= $this->getChildrenHtml($children);
-      }else{
-        $subhtml .= 'asdasdasdasdasdas';
-      }
-    }
-    $subhtml .= '</ul>';
-    return $subhtml;
-	}
 	/**
 	 * GetChildren the form for editing the specified resource.
 	 * GET /categorias/{id}/edit
@@ -135,20 +81,22 @@ class CategoriasController extends BaseController {
 	 */
   public function getChildren($children) 
   {
+    $subhtml = '<ul class="list-group">';
     foreach($children as $child){
-      $child_categoria = array();
-      $child_categoria['id'] = $child->id;
-      $child_categoria['name'] = $child->name;
-      $children = DB::table('categorias')->where('parent_id', '=', $child->id)->get();
+			
+			$children = DB::table('categorias')->where('parent_id', '=', $child->id)->get();
+  		$subhtml .= '<li class="list-group-item">';
+  		$subhtml .= '<span class="badge">'.count($children).'</span>';
+  		$subhtml .= '<a href="/delete/'.$child->id.'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> </a>';
+			$subhtml .= $child->name;
 
-      if(count($children > 0)) {
-        $child_categoria['children'] = $this->getChildren($children);
-      }else{
-        $child_categoria['children'] = array();
+      if(count($children) > 0) {
+        $subhtml .= $this->getChildren($children);
       }
 
-      return $child_categoria;
     }
+    $subhtml .= '</ul>';
+    return $subhtml;
 	}
 
 	/**
