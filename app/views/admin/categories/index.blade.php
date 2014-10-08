ab062b53ae59a2af4e30aba1914d562e8e347783
Damián Marcote <dmarcote2004@yahoo.com.ar>
2014-10-07 23:56:41 -0300

    Revert a6aef8d..3619b1a
    This rolls back to commit a6aef8d504e8d09cd6065e9f25660a0261417659.

--- app/views/admin/categories/index.blade.php
+++ app/views/admin/categories/index.blade.php
@@ -2,52 +2,40 @@
 
 @section('content')
 
-	<div class="row">
-	  <div class="col-xs-12 col-md-12">
-			<h1>Categorías <small>Usted puede ver, borrar y crear nuevas Categorías.</small></h1><hr>
-	  </div>
-	  <div class="col-xs-12 col-md-12">
-			<div class="row">
-			  <div class="col-xs-8 col-md-8">
-			  	<h3>Listado de Categorías - Eliminar</h3>
-			  </div>
-			  <div class="col-xs-4 col-md-4">
-					<br><a href="/admin/categories/create/list" class="btn btn-success pull-right">Crear Categoría</a>
-			  </div>
-			</div>
-			<hr>
-	  </div>
-	  <div class="col-xs-12 col-md-12">
-			{{ $list }}
-	  </div>
-	  <div class="col-xs-12 col-md-12">
-	  	
-	  	<h3>Crear Nueva Categoría</h3><hr>
-			
-			@if($errors->has())
-			<div class="alert alert-danger" role="alert">
-				<p>Ocurrieron los siguientes errores:</p>
-				<ul>
-					@foreach($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-			@endif
+	<h1>Categorias</h1><hr>
 
-			{{ Form::open(array( 'url' => '/admin/categories/create' , 'method' => 'get' )) }}
-			<p>
-				<label for="nombre">Nombre</label>
-				<input type="text" name="name" class="form-control" placeholder="Nombre de la nueva Categoría">
-			</p>
-			<p>
-				<label for="nombre">Seleccione la pertenencia de la nueva categoría. Si no selecciona ninguna opción, creará una Categoría Padre.</label>
-				{{ $select }}
-			</p>
-			{{ Form::submit('Crear Categoría', array('class'=>'btn btn-success')) }}
-			{{ Form::close() }}
-			
-	  </div>
-	</div>
+	<p>Usted puede ver, borrar y crear nuevas Categorías.</p>
+
+	<h2>Listado de Categorias existentes</h2><hr>
+
+	{{ $list }}
+
+	<h2>Crear Nueva Categoría</h2><hr>
+
+	@if($errors->has())
+	<div class="alert alert-danger" role="alert">
+		<p>Ocurrieron los siguientes errores:</p>
+		<ul>
+			@foreach($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div><!-- end form-errors -->
+	@endif
+
+
+	{{ Form::open(array( 'url' => '/admin/categories/create' , 'method' => 'get' )) }}
+	<p>
+		<label for="nombre">Pertenece a:</label>
+		{{ $select }}
+		<!-- {{ Form::select('category_id', Category::lists('name','id'), array('class' => 'form-control')) }} -->
+	</p>
+	<p>
+		<label for="nombre">Nombre</label>
+		<input type="text" name="name" class="form-control" placeholder="Nombre de la Categoría">
+	</p>
+	{{ Form::submit('Crear Categoría', array('class'=>'btn btn-success')) }}
+	{{ Form::close() }}
+
 
 @stop
