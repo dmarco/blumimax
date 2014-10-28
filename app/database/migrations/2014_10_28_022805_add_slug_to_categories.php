8576903aeacc9d3ab160597ab545cc7d2ae6e813
Damián Marcote <dmarcote2004@yahoo.com.ar>
2014-10-27 23:41:43 -0300

    Add: slug to categories

---
+++ app/database/migrations/2014_10_28_022805_add_slug_to_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlugToCategories extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('categories', function($table){
+			$table->string('slug', 255);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('categorias');
+	}
+
+}
